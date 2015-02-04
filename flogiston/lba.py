b244a7285886dd27b5d2b770f13d43fb8b079b64
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2015-02-04 21:59:09 +1100

    Enhanced LBA-BOLD functions

--- flogiston/lba.py
+++ flogiston/lba.py
@@ -3,6 +3,7 @@ from utils import dnormP, pnormP
 import numpy as np
 from sampler import Node
 from bold import simulate_bold, get_likelihood_hrf
+from utils import pack_conditions_responses_param
 
 def pdf(ter, A, v, sv, b, t):
     """LBA PDF for a single accumulator"""
@@ -63,12 +64,11 @@ def generate_lba_data(ters, As, vs, svs, bs, bold=None, height_intercept=1.0, de
         if bold == 'first_drift':
             expression = zv[:, 0] - vs[0]
         if bold == 'winning_drift':
-            print zv.shape, zv[np.arange(zv.shape[0]), responses-1].shape, vs[responses-1].shape
             expression = zv[np.arange(zv.shape[0]), responses-1] - vs[responses-1]
         elif bold == 'total_drift':
             expression = zv.sum(1) - vs.sum()
         elif bold == 'winning_distance':
-            expression = -zs[:,0] - As[0]/2 #bs[0] - zs[:,0] - bs[0] - A/2
+            expression = -zs[np.arange(zs.shape[0]),responses-1] - As[responses-1]/2 #bs[0] - zs[:,0] - bs[0] - A/2
         elif bold == 'first_distance':
             expression = zs[:,0]#bs[0] - zs[:,0] - bs[0] - A/2
         elif bold == 'total_distance':
@@ -110,7 +110,6 @@ def likelihood(responses, RTs, ters, As, vs, svs, Bs, n_responses=2, n_condition
     
     for resp_idx in np.arange(1, n_responses+1):
         for c in np.arange(1, n_conditions+1):
-            #print responses, (responses == resp_idx) & (conditions == c)
             resp = responses[(responses == resp_idx) & (conditions == c)] 
             RT = RTs[(responses == resp_idx) & (conditions == c)] 
 
@@ -144,29 +143,22 @@ def plot_likelihood(ters, As, vs, svs, bs, t_max=2.0, n_ts=100, color_palette='S
         plt.plot(t, np.exp(likelihood(np.array([resp] * len(t)), t, ters, As, vs, svs, bs, return_individual_ll=True)), color=color, **kwargs)
 
 
-def bold_likelihood(responses, RTs, conditions, bold, frametimes, onsets, ter, v, A, B, kind='winning_drift', sv=None, height_intercept=1.0, delay_intercept=6.0, height_theta=1.0, delay_theta=0.0, n_conditions=1):
- 
+def bold_likelihood(responses, RTs, conditions, bold, frametimes, onsets, ter=None, v=None, A=None, B=None, kind='winning_drift', sv=None, height_intercept=1.0, delay_intercept=6.0, height_theta=1.0, delay_theta=0.0, n_conditions=1, n_responses=2):
 
     if sv == None:
         sv = np.ones_like(ter)
 
     if kind == 'winning_drift':
-        print 'yo'
-        expressions = np.zeros_like(responses)
 
         if np.all(A == 0):
-
-            T = RTs - ter[responses - 1].ravel()
-            emp_v = B[responses-1].ravel() / T
-            
-            #pd = stats.norm.pdf(x=emp_v, loc=v[responses-1], scale=sv[responses-1])
-            
+            T = RTs - ter[responses - 1, conditions - 1]
+            emp_v = B[responses-1, conditions-1] / T
             expression = emp_v - v[responses-1, conditions-1]
+    
+    heights = height_intercept[responses - 1, conditions -1] + expression * height_theta[responses - 1, conditions -1]
+    delays = delay_intercept[responses - 1, conditions -1] + expression * delay_theta[responses - 1, conditions -1]
 
-    heights = height_intercept + expression * height_theta
-    delays = delay_intercept + expression * delay_theta
-
-    return get_likelihood_hrf(bold, frametimes, onsets, heights, peak_delays=delays)
+    return get_likelihood_hrf(bold, frametimes, onsets, heights, dispersions=1, peak_delays=delays)
 
 
 class LBA(Node):
@@ -190,18 +182,13 @@ class LBA(Node):
         else:
             self.n_conditions = n_conditions
             self.n_responses = n_responses
+       
+        ter = pack_conditions_responses_param(ter, self.n_conditions, self.n_responses)
+        v = pack_conditions_responses_param(v, self.n_conditions, self.n_responses)
+        A = pack_conditions_responses_param(A, self.n_conditions, self.n_responses)
+        B = pack_conditions_responses_param(B, self.n_conditions, self.n_responses)
+        sv = pack_conditions_responses_param(sv, self.n_conditions, self.n_responses)
         
-        if type(ter) != list:
-            ter = [[ter] * self.n_conditions] * self.n_responses
-        if type(v) != list:
-            v = [[v] * self.n_conditions] * self.n_responses
-        if type(A) != list:
-            A = [[A] * self.n_conditions] * self.n_responses
-        if type(B) != list:
-            B = [[B] * self.n_conditions] * self.n_responses
-        if type(sv) != list:
-            sv = [[sv] * self.n_conditions] * self.n_responses
-
         parents = {}
 
         self.parent_node_keys = {}
@@ -220,7 +207,6 @@ class LBA(Node):
     
     def _likelihood(self, values):
         parameter_values = dict([(par, [[self.parents[t].get_value() for t in sl] for sl in self.parent_node_keys[par]]) for par in LBA.pars])
-        #print parameter_values
         return likelihood(values[0], 
                           values[1],
                           parameter_values['ter'],
@@ -232,4 +218,70 @@ class LBA(Node):
                           n_conditions=self.n_conditions,
                           conditions=values[2])
 
-#responses, RTs, ters, As, vs, svs, bs, n_responses=2, n_conditions=None, conditions=None, return_individual_ll=False
+class LBA_BOLD(Node):
+
+    pars = ['ter', 'B', 'A', 'v', 'sv', 'height_intercept', 'height_theta', 'delay_intercept', 'delay_theta']
+    
+    def __init__(self, name, ter, v, A, B, height_intercept=1.0, height_theta=0.0, delay_intercept=6.0, delay_theta=0.0, sv=1.0, n_conditions=None, n_responses=None, value=(None, None, None, None, None, None), kind='winning_drift', **kwargs):
+        " This Node expects lists of nodes as parents!!"
+        " Example: [[ter_resp1_cond1, ter_resp1_cond2], [ter_resp2_cond1, ter_resp2_cond2]] "
+        " Value (responses, RT, conditions, bold, frametimes, onsets) "
+        
+        self.stochastic = False
+        self.children = []
+
+        self.kind = kind
+
+        if not n_conditions or not n_responses:
+            for l in [ter, v, A, B]:
+                if type(l) == list:
+                    self.n_responses = len(l)
+                    self.n_conditions = len(l[0])
+                    break
+            raise Exception("Don't know number of conditions!")
+        else:
+            self.n_conditions = n_conditions
+            self.n_responses = n_responses
+        
+        ter = pack_conditions_responses_param(ter, self.n_conditions, self.n_responses)
+        v = pack_conditions_responses_param(v, self.n_conditions, self.n_responses)
+        A = pack_conditions_responses_param(A, self.n_conditions, self.n_responses)
+        B = pack_conditions_responses_param(B, self.n_conditions, self.n_responses)
+        sv = pack_conditions_responses_param(sv, self.n_conditions, self.n_responses)
+        height_intercept = pack_conditions_responses_param(height_intercept, self.n_conditions, self.n_responses)
+        height_theta = pack_conditions_responses_param(height_theta, self.n_conditions, self.n_responses)
+        delay_intercept = pack_conditions_responses_param(delay_intercept, self.n_conditions, self.n_responses)
+        delay_theta = pack_conditions_responses_param(delay_theta, self.n_conditions, self.n_responses)
+
+        parents = {}
+
+        self.parent_node_keys = {}
+
+        for par in LBA_BOLD.pars:
+            self.parent_node_keys[par] = []
+            for resp_idx in np.arange(0, self.n_responses):
+                self.parent_node_keys[par].insert(resp_idx, [])
+                for cond_idx in np.arange(0, self.n_conditions):
+                    key = '%s.resp_%d.condition_%d' % (par, resp_idx+1, cond_idx+1)
+                    self.parent_node_keys[par][resp_idx].insert(cond_idx, key)
+                    parents[key] = locals()[par][resp_idx][cond_idx]
+
+        Node.__init__(self, name, parents, value=value, **kwargs )
+
+    
+    def _likelihood(self, values):
+        parameter_values = dict([(par, np.array([[self.parents[t].get_value() for t in sl] for sl in self.parent_node_keys[par]])) for par in LBA_BOLD.pars])
+        return bold_likelihood(values[0], 
+                          values[1],
+                          values[2],
+                          values[3],
+                          values[4],
+                          values[5],
+                          n_responses=self.n_responses,
+                          n_conditions=self.n_conditions,
+                          kind=self.kind,
+                          **parameter_values)
+
+
+#def bold_likelihood(responses, RTs, conditions, bold, frametimes, onsets, ter=None, v=None, A=None, B=None, kind='winning_drift', sv=None, height_intercept=1.0, delay_intercept=6.0, height_theta=1.0, delay_theta=0.0, n_conditions=1, n_responses=2):
+ 
